237f1b2a1ad8d4185ee54311f8cdd6288ed0c213
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-08-15 17:14:43 +1000

    implemented sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,114 @@
-import { Link } from '@tanstack/react-router'
+import { useState } from 'react'
+import { Sidebar, SidebarBody, SidebarLink } from '@/components/ui/sidebar'
+import {
+  IconFileDescription,
+  IconBooks,
+  IconHome,
+  IconMusic,
+} from '@tabler/icons-react'
+import { motion } from 'motion/react'
+import { cn } from '@/lib/utils'
 
-export default function Header() {
+export function Header({ children }: { children?: React.ReactNode }) {
+  const links = [
+    {
+      label: 'Home',
+      href: '/',
+      icon: (
+        <IconHome className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: 'Your Library',
+      href: '#',
+      icon: (
+        <IconBooks className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: 'Playlists',
+      href: '#',
+      icon: (
+        <IconMusic className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
+      label: 'Documents',
+      href: '#',
+      icon: (
+        <IconFileDescription className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+  ]
+  const [open, setOpen] = useState(true)
   return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
+    <div
+      className={cn(
+        'mx-auto flex w-full flex-1 flex-col overflow-hidden border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800',
+        'h-screen',
+      )}
+    >
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+            {open ? <Logo /> : <LogoIcon />}
+            <div className="mt-8 flex flex-col gap-2">
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <SidebarLink
+              link={{
+                label: 'Manu Arora',
+                href: '#',
+                icon: (
+                  <img
+                    src="https://assets.aceternity.com/manu.png"
+                    className="h-7 w-7 shrink-0 rounded-full"
+                    width={50}
+                    height={50}
+                    alt="Avatar"
+                  />
+                ),
+              }}
+            />
+          </div>
+        </SidebarBody>
+      </Sidebar>
+      <div className="flex flex-1">
+        <div className="flex h-full w-full flex-1 flex-col gap-2 border border-neutral-200 bg-white p-2 md:p-10 dark:border-neutral-700 dark:bg-neutral-900 text-white">
+          {children}
         </div>
-      </nav>
-    </header>
+      </div>
+    </div>
+  )
+}
+export const Logo = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium whitespace-pre text-black dark:text-white"
+      >
+        Acet Labs
+      </motion.span>
+    </a>
+  )
+}
+export const LogoIcon = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" />
+    </a>
   )
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,17 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanstackDevtools } from '@tanstack/react-devtools'
+// import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+// import { TanstackDevtools } from '@tanstack/react-devtools'
 
-import Header from '../components/Header'
+import { Header } from '../components/Header'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Header />
-      <Outlet />
-      <TanstackDevtools
+      <Header>
+        <Outlet />
+      </Header>
+
+      {/* <TanstackDevtools
         config={{
           position: 'bottom-left',
         }}
@@ -19,7 +21,7 @@ export const Route = createRootRoute({
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
-      />
+      /> */}
     </>
   ),
 })
