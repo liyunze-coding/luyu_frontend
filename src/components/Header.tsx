760bc52f00a486d64d19fd51e8887c3229563a2b
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-08-16 12:26:53 +1000

    updated header scroll error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,8 +45,8 @@ export function Header({ children }: { children?: React.ReactNode }) {
 	return (
 		<div
 			className={cn(
-				"mx-auto flex w-full flex-1 flex-col overflow-hidden border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
-				"h-screen",
+				"mx-auto flex w-full flex-1 flex-col border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
+				"min-h-screen",
 			)}
 		>
 			<Sidebar open={open} setOpen={setOpen}>
