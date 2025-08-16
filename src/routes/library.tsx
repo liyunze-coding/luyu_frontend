54be990cb412d7c308cdc2e6475e1bae6c070aa9
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-08-16 14:13:49 +1000

    added playlists component

--- src/routes/library.tsx
+++ src/routes/library.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Playlist } from "@/components/your-library/Playlists";
 
 export const Route = createFileRoute("/library")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/library"!</div>;
+	return (
+		<div className="w-full px-10 py-10">
+			<h1 className="mb-2 text-4xl font-bold">Your Library</h1>
+			<p className="text-lg text-gray-400">
+				Manage your audio content and playlists
+			</p>
+			<Playlist />
+		</div>
+	);
 }
